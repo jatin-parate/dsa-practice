51849a2ab6c2c87b4a7e8344325978846a485bb1
Jatin <jatin.parate@mlveda.com>
2021-08-30 12:08:01 +0530

    initialized repo

--- array/median-of-two-sorted-arrays.js
+++ array/median-of-two-sorted-arrays.js
@@ -3,47 +3,47 @@
  * @param {number[]} nums2
  * @return {number}
  */
-var findMedianSortedArrays = function(nums1, nums2) {
+// eslint-disable-next-line no-unused-vars
+const findMedianSortedArrays = function findMedianSortedArrays(nums1, nums2) {
   // pointer to element of nums1
   let i = 0;
   // pointer to element of num2
   let j = 0;
-  let finalArray = []
+  const finalArray = [];
 
   // start combining both the arrats
-  while(i < nums1.length && j < nums2.length) {
+  while (i < nums1.length && j < nums2.length) {
     if (nums1[i] <= nums2[j]) {
       finalArray.push(nums1[i]);
-      i++;
+      i += 1;
     } else {
       finalArray.push(nums2[j]);
-      j++;
+      j += 1;
     }
   }
 
   if (i < nums1.length) {
-    for (; i < nums1.length; i++) {
-      finalArray.push(nums1[i])
+    for (; i < nums1.length; i += 1) {
+      finalArray.push(nums1[i]);
     }
   }
 
   if (j < nums2.length) {
-    for(; j < nums2.length; j++) {
-      finalArray.push(nums2[j])
+    for (; j < nums2.length; j += 1) {
+      finalArray.push(nums2[j]);
     }
   }
 
   if (finalArray.length === 1) {
-    return finalArray[0]
+    return finalArray[0];
   }
 
   const middle = finalArray.length / 2;
 
   if (finalArray.length % 2 === 0) {
     // even
-    return (finalArray[middle - 1] + finalArray[middle]) / 2
-  } else {
-    // odd
-    return finalArray[Math.floor(middle)]
+    return (finalArray[middle - 1] + finalArray[middle]) / 2;
   }
+  // odd
+  return finalArray[Math.floor(middle)];
 };
