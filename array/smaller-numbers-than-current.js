6cd596863551917c2e662ecfd67be06d92a3ed66
Jatin <jatin.parate@mlveda.com>
2021-11-10 20:14:22 +0530

    updated 1365

--- array/smaller-numbers-than-current.js
+++ array/smaller-numbers-than-current.js
@@ -4,24 +4,14 @@
  */
 // eslint-disable-next-line no-unused-vars
 const smallerNumbersThanCurrent = (nums) => {
-  const map = new Map();
+  const freq = new Array(101).fill(0);
+  const ans = new Array(101).fill(0);
 
-  nums.forEach((num) => {
-    if (!map.has(num)) {
-      map.set(num, 0);
-    }
-    map.set(num, map.get(num) + 1);
-  });
+  nums.forEach((num) => { freq[num] += 1; });
 
-  const result = new Array(nums.length).fill(0);
+  for (let i = 1; i < 101; i += 1) {
+    ans[i] = ans[i - 1] + freq[i - 1];
+  }
 
-  nums.forEach((num, index) => {
-    map.forEach((val, key) => {
-      if (key < num) {
-        result[index] += val;
-      }
-    });
-  });
-
-  return result;
+  return nums.map((num) => ans[num]);
 };
