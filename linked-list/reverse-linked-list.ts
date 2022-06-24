7c091ad922d73c383cf1951f9781d95d9cf13980
Jatin <jatin.parate@mlveda.com>
2022-06-24 14:41:18 +0530

    reverse a linked list

---
+++ linked-list/reverse-linked-list.ts
@@ -0,0 +1,28 @@
+import { List } from "immutable";
+
+class ListNode {
+  constructor(public value: number, public next: ListNode | null = null) {}
+}
+
+const reverseList = (head: ListNode | null): ListNode | null => {
+  let prev = null;
+
+  while (head) {
+    const temp = head.next;
+    head.next = prev;
+    prev = head;
+    head = temp;
+  }
+  return prev;
+};
+
+let head = new ListNode(
+  5,
+  new ListNode(4, new ListNode(3, new ListNode(2, new ListNode(1))))
+);
+
+console.log(JSON.stringify(reverseList(head), null, 2));
+// console.log(JSON.stringify(reverseList(new ListNode(1)), null, 2));
+// console.log(
+//   JSON.stringify(reverseList(new ListNode(1, new ListNode(2))), null, 2)
+// );
