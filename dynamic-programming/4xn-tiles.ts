403f08f2d7b80b1db668cd8cccd74c3f082bd189
Jatin <jatin.parate@mlveda.com>
2022-07-11 09:33:39 +0530

    4xn tiles tabulated

--- dynamic-programming/4xn-tiles.ts
+++ dynamic-programming/4xn-tiles.ts
@@ -10,7 +10,17 @@ const totalWays = (n: number, dp: number[] = []): number => {
   return (dp[n] = totalWays(n - 1, dp) + totalWays(n - 4, dp));
 };
 
-console.log(totalWays(4));
-console.log(totalWays(5));
-console.log(totalWays(6));
-console.log(totalWays(10));
+const totalWaysTabulated = (n: number): number => {
+  const table = new Array<number>(n + 1).fill(1);
+
+  for (let i = 4; i <= n; i += 1) {
+    table[i] = table[i - 1] + table[i - 4];
+  }
+
+  return table[n];
+};
+
+console.log(totalWays(4), totalWaysTabulated(4));
+console.log(totalWays(5), totalWaysTabulated(5));
+console.log(totalWays(6), totalWaysTabulated(6));
+console.log(totalWays(10), totalWaysTabulated(10));
