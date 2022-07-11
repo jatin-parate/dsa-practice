c65f7ca5af000dda76870b627ca3728298691471
Jatin <jatin.parate@mlveda.com>
2022-07-11 09:30:47 +0530

    4xn tiles with caching

--- dynamic-programming/4xn-tiles.ts
+++ dynamic-programming/4xn-tiles.ts
@@ -1,11 +1,16 @@
-const totalWays = (n: number): number => {
+const totalWays = (n: number, dp: number[] = []): number => {
   if (n < 4) {
     return 1;
   }
 
-  return totalWays(n - 1) + totalWays(n - 4);
+  if (dp[n] !== undefined) {
+    return dp[n];
+  }
+
+  return (dp[n] = totalWays(n - 1, dp) + totalWays(n - 4, dp));
 };
 
 console.log(totalWays(4));
 console.log(totalWays(5));
 console.log(totalWays(6));
+console.log(totalWays(10));
