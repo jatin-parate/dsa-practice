c61e5a7fc46e12a5e75752aecccef3fe2531e3aa
Jatin <jatin.parate@mlveda.com>
2022-12-04 18:04:29 +0530

    Optimized 560-subarray-equals-k brute force for only non-negative arrays

--- leet-code/560-subarray-equals-k.ts
+++ leet-code/560-subarray-equals-k.ts
@@ -1,4 +1,6 @@
 export function subarraySum(nums: number[], k: number): number {
+  const min = Math.min(...nums);
+  const hasNegative = min < 0;
   let total = 0;
   const n = nums.length;
   for (let starting = 0; starting < n; starting++) {
@@ -11,13 +13,18 @@ export function subarraySum(nums: number[], k: number): number {
       if (currentSum === k) {
         total += 1;
       }
-      i++;
+      if (!hasNegative && currentSum > k) {
+        break;
+      } else {
+        i++;
+      }
     }
   }
 
   return total;
 }
 
+console.log(subarraySum([1, 2, 1, 2, 1], 3));
 console.log(subarraySum([1, 1, 1], 2));
 console.log(subarraySum([1, 2, 3], 3));
 console.log(subarraySum([1, -1, 0], 0));
