1be6b0d514823d1f36a8e1fe17070b882cde7db4
Jatin <jatin.parate@mlveda.com>
2022-12-17 21:27:33 +0530

    220 contains duplicate 3 improved

--- sliding-window/220-contains-duplicates-3.ts
+++ sliding-window/220-contains-duplicates-3.ts
@@ -4,11 +4,8 @@ export function containsNearbyAlmostDuplicate(
   valueDiff: number
 ): boolean {
   for (let i = 0; i < nums.length - 1; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (
-        Math.abs(i - j) <= indexDiff &&
-        Math.abs(nums[i] - nums[j]) <= valueDiff
-      ) {
+    for (let j = i + 1; j <= i + indexDiff && j < nums.length; j++) {
+      if (Math.abs(nums[i] - nums[j]) <= valueDiff) {
         return true;
       }
     }
@@ -17,5 +14,7 @@ export function containsNearbyAlmostDuplicate(
   return false;
 }
 
+console.log(containsNearbyAlmostDuplicate([1, 2, 5, 6, 7, 2, 4], 4, 0));
+console.log(containsNearbyAlmostDuplicate([4,1,-1,6,5], 3, 1));
 console.log(containsNearbyAlmostDuplicate([1, 2, 3, 1], 3, 0));
 console.log(containsNearbyAlmostDuplicate([1, 5, 9, 1, 5, 9], 2, 3));
