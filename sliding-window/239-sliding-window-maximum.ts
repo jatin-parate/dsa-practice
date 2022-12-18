6791cc4130fdae82a673a1b163f040c49b0e8c57
Jatin <jatin.parate@mlveda.com>
2022-12-18 13:01:40 +0530

    improved sliding-window-maximum

--- sliding-window/239-sliding-window-maximum.ts
+++ sliding-window/239-sliding-window-maximum.ts
@@ -1,33 +1,53 @@
 export function maxSlidingWindow(nums: number[], k: number): number[] {
   let start = 0,
     end = start + k - 1;
-  let currMaxIndex = -1;
-  const res: number[] = [];
-
-  function calculateCurrMaxIndex(): void {
-    currMaxIndex = start;
-    for (let i = start + 1; i <= end; i++) {
-      if (nums[i] > nums[currMaxIndex]) {
-        currMaxIndex = i;
-      }
-    }
-  }
+  let result: number[] = [];
 
   while (end < nums.length) {
-    if (currMaxIndex < start) {
-      calculateCurrMaxIndex();
-    } else if (nums[end] > nums[currMaxIndex]) {
-      currMaxIndex = end;
+    let currentMaxIndex = start;
+    for (let i = start + 1; i <= end; i++) {
+      if (nums[i] > nums[currentMaxIndex]) currentMaxIndex = i;
     }
 
-    res.push(nums[currMaxIndex]);
+    // find next maximum in the range of currentMaxIndex
+    let nextMaxIndex = -1;
+    for (
+      let i = currentMaxIndex + 1;
+      i < nums.length && i < currentMaxIndex + k;
+      i++
+    ) {
+      if (nums[i] > nums[currentMaxIndex]) {
+        nextMaxIndex = i;
+        break;
+      }
+    }
 
-    // === must increment indexes at the end ===
-    start++;
-    end++;
+    // console.log("going in loop");
+
+    while (
+      end < nums.length &&
+      start <= currentMaxIndex &&
+      (nextMaxIndex === -1 || end < nextMaxIndex)
+    ) {
+      // console.log(
+      //   nums[currentMaxIndex],
+      //   ":",
+      //   start,
+      //   "to",
+      //   end,
+      //   "with curr max index",
+      //   currentMaxIndex,
+      //   "and next max index",
+      //   nextMaxIndex
+      // );
+
+      result.push(nums[currentMaxIndex]);
+      start++;
+      end++;
+    }
   }
 
-  return res;
+  return result;
 }
 
 console.log(maxSlidingWindow([1, 3, -1, -3, 5, 3, 6, 7], 3));
