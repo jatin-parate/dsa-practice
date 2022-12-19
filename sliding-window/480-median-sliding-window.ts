1ec9217902ce7e02a1d67bd6d9419a876b25a3b4
Jatin <jatin.parate@mlveda.com>
2022-12-19 14:13:25 +0530

    optimized median sliding window

--- sliding-window/480-median-sliding-window.ts
+++ sliding-window/480-median-sliding-window.ts
@@ -1,16 +1,35 @@
 export function medianSlidingWindow(nums: number[], k: number): number[] {
   const result: number[] = [];
-  for (let i = 0; i <= nums.length - k; i++) {
-    const arr = nums.slice(i, i + k);
-    arr.sort((a, b) => a - b);
-    console.log(arr);
+  const isKEven = k % 2 === 0;
+  const tmp: number[] = nums.slice(0, k);
+  tmp.sort((a, b) => a - b);
 
-    if (k % 2 === 0) {
-      const tmp = k / 2;
-      result.push((arr[tmp] + arr[tmp - 1]) / 2);
+  const insertMedian = () => {
+    if (isKEven) {
+      const mid = k / 2;
+      result.push((tmp[mid] + tmp[mid - 1]) / 2);
     } else {
-      result.push(arr[Math.floor(k / 2)]);
+      result.push(tmp[Math.floor(k / 2)]);
     }
+  };
+
+  insertMedian();
+
+  for (let i = 1; i <= nums.length - k; i++) {
+    let index = tmp.indexOf(nums[i - 1]);
+    tmp.splice(index, 1);
+
+    if (nums[i + k - 1] < tmp[0]) {
+      tmp.unshift(nums[i + k - 1]);
+    } else {
+      let index = 0;
+      while (index < tmp.length && tmp[index] < nums[i + k - 1]) {
+        index++;
+      }
+      tmp.splice(index, 0, nums[i + k - 1]);
+    }
+
+    insertMedian();
   }
 
   return result;
