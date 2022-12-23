e1510d837938d5e14c71b0635a855b83509ee363
Jatin <jatin.parate@mlveda.com>
2022-12-23 12:15:12 +0530

    maximum sum of distinct subarrays with length k optimized

--- sliding-window/2461-max-sum-of-distinct-subarrays-with-length-k.ts
+++ sliding-window/2461-max-sum-of-distinct-subarrays-with-length-k.ts
@@ -1,26 +1,31 @@
 export function maximumSubarraySum(nums: number[], k: number): number {
-    const map = new Map<number, number>();
-    let max = 0;
-    let currSum = 0;
-    for (let i = 0; i < nums.length; i++) {
-        map.set(nums[i], 1 + (map.get(nums[i]) ?? 0));
-        currSum += nums[i];
-        // time to delete previous element at starting of window
-        if (i >= k) {
-            const index = i - k;
-            currSum -= nums[index];
-            const freq = map.get(nums[index]) ?? 0;
-            if (freq !== 0) {
-                map.set(nums[index], freq - 1);
-            }
-        } else if (i < k - 1) {
-            continue;
-        }
-
-        if ([...map.values()].every(val => val < 2)) {
-            max = Math.max(max, currSum);
-        }
+  const map = new Map<number, number>();
+  let max = 0;
+  let currSum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    map.set(nums[i], 1 + (map.get(nums[i]) ?? 0));
+    currSum += nums[i];
+    // time to delete previous element at starting of window
+    if (i >= k) {
+      const index = i - k;
+      currSum -= nums[index];
+      const freq = map.get(nums[index])!;
+      if (freq > 1) {
+        map.set(nums[index], freq - 1);
+      } else {
+        map.delete(nums[index]);
+      }
+    } else if (i < k - 1) {
+      continue;
     }
 
-    return max;
-};
+    if (map.size === k) {
+      max = Math.max(max, currSum);
+    }
+  }
+
+  return max;
+}
+
+console.log(maximumSubarraySum([1, 5, 4, 2, 9, 9, 9], 3)); // 15
+console.log(maximumSubarraySum([4, 4, 4], 3)); // 15
