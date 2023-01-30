4beb40112bd3d544ca77efb2418847d2a9439daa
Jatin <jatin.parate@mlveda.com>
2023-01-30 11:19:46 +0530

    triangle optimized

--- trees/120-triangle.ts
+++ trees/120-triangle.ts
@@ -1,12 +1,26 @@
-export function minimumTotal(triangle: number[][], i = 0, j = 0): number {
+export function minimumTotal(
+  triangle: number[][],
+  cache: number[][] = [],
+  i = 0,
+  j = 0
+): number {
   if (i >= triangle.length) return 0;
+  if (cache[i]?.[j] !== undefined) {
+    return cache[i][j];
+  }
 
   let sum =
     triangle[i][j] +
     Math.min(
-      minimumTotal(triangle, i + 1, j),
-      minimumTotal(triangle, i + 1, j + 1)
+      minimumTotal(triangle, cache, i + 1, j),
+      minimumTotal(triangle, cache, i + 1, j + 1)
     );
+
+  if (!cache[i]) {
+    cache[i] = new Array(triangle[i].length).fill(undefined);
+  }
+
+  cache[i][j] = sum;
 
   return sum;
 }
