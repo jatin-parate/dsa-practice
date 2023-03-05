07a52bb9c09e91cea55c6ab52815e15fd7d69837
Jatin <jatin.parate@mlveda.com>
2023-03-05 17:50:02 +0530

    optimized lowest common ancestor

--- b-tree/235-lowest-common-ancestor-of-binary-search-tree.ts
+++ b-tree/235-lowest-common-ancestor-of-binary-search-tree.ts
@@ -1,50 +1,14 @@
 import { TreeNode } from "../trees/utils";
 
-type Arr = [boolean, boolean];
-function mergeBoolArray(first: Arr, second: Arr): void {
-  first[0] = first[0] || second[0];
-  first[1] = first[1] || second[1];
-}
-
 export function lowestCommonAncestor(
   root: TreeNode | null,
-  p: TreeNode | null,
-  q: TreeNode | null
+  p: TreeNode,
+  q: TreeNode
 ): TreeNode | null {
-  if (!root) return root;
-  let isFound = false;
-  let ancestor: TreeNode | null = null;
-
-  function dfs(node: TreeNode | null): Arr {
-    if (isFound) return [true, true];
-    if (!node) return [false, false];
-
-    let res: Arr = [false, false];
-    if (node === p) res[0] = true;
-    if (node === q) res[1] = true;
-
-    mergeBoolArray(res, dfs(node.left));
-
-    if (isFound) return [true, true];
-
-    if (res[0] && res[1]) {
-      ancestor = node;
-      isFound = true;
-      return res;
-    }
-
-    mergeBoolArray(res, dfs(node.right));
-
-    if (isFound) return [true, true];
-
-    if (res[0] && res[1]) {
-      ancestor = node;
-      isFound = true;
-    }
-
-    return res;
+  while (root) {
+    if (root.val < p.val && root.val < q.val) root = root.right;
+    else if (root.val > p.val && root.val > q.val) root = root.left;
+    else break;
   }
-
-  dfs(root);
-  return ancestor;
+  return root;
 }
